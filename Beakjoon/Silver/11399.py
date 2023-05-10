6c962f43cfc6fd94561ce6809ed253aa0fd03ed8
jiggy97 <dkswnsdud159@naver.com>
2023-05-10 17:05:09 +0900

    [python] Beakjoon - [silver] 11399

---
+++ Beakjoon/Silver/11399.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+p = list(map(int, input().split()))
+p.sort()
+
+time = 0
+tmp = 0
+for i in p:
+    tmp += i
+    time += tmp
+
+print(time)
---
+++ Beakjoon/Silver/2667.py
@@ -0,0 +1,18 @@
+n = int(input())
+num_house = 0
+
+
+def dfs(graph, start, visited, count):
+    visited[start] =
+
+    return count
+
+
+map_data = []
+for i in range(n):
+    row = list(map(int, input()))
+    map_data.append(row)
+
+count_house = [[] for i in range(n)]
+
+dfs(map_data, (0, 0), count_house, num_house)
