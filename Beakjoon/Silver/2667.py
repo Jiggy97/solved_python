cc6da819146d622fac30e9a898ca078e746b9fa3
jiggy97 <dkswnsdud159@naver.com>
2023-05-18 21:04:11 +0900

    [python] Beakjoon - [silver] 2667 단지번호붙이기

--- Beakjoon/Silver/2667.py
+++ Beakjoon/Silver/2667.py
@@ -1,18 +1,52 @@
-n = int(input())
-num_house = 0
+from collections import deque
 
+# 상, 하, 좌, 우 방향 설정
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def dfs(graph, start, visited, count):
-    visited[start] =
+def bfs(x, y, graph, visited, n):
+    count = 1
+    queue = deque([(x, y)])
+    visited[x][y] = True
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+
+            if graph[nx][ny] == 1 and not visited[nx][ny]:
+                queue.append((nx, ny))
+                visited[nx][ny] = True
+                count += 1
 
     return count
 
+def solution(n, graph):
+    visited = [[False] * n for _ in range(n)]
+    result = []
 
-map_data = []
-for i in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] == 1 and not visited[i][j]:
+                result.append(bfs(i, j, graph, visited, n))
+
+    result.sort()
+    return result
+
+# 입력 예시
+n = int(input())
+graph = []
+for _ in range(n):
     row = list(map(int, input()))
-    map_data.append(row)
+    graph.append(row)
 
-count_house = [[] for i in range(n)]
-
-dfs(map_data, (0, 0), count_house, num_house)
+# 결과 출력
+answer = solution(n, graph)
+print(len(answer))
+for count in answer:
+    print(count)
