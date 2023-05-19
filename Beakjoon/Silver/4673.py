b98ead7c4980d63ae1af1b30aedd4e586dc1c425
jiggy97 <dkswnsdud159@naver.com>
2023-05-19 18:48:25 +0900

    [python] Beakjoon - [silver] 4673 셀프 넘버

---
+++ Beakjoon/Silver/4673.py
@@ -0,0 +1,24 @@
+def d(n):
+    next_num = n
+    while n > 0:
+        next_num += n % 10
+        n //= 10
+    return next_num
+
+def solution():
+    numbers = set(range(1, 10001))
+    self_numbers = set()
+
+    for num in numbers:
+        next_num = d(num)
+        while next_num <= 10000:
+            self_numbers.add(next_num)
+            next_num = d(next_num)
+
+    result = numbers - self_numbers
+    return sorted(result)
+
+# 결과 출력
+answer = solution()
+for num in answer:
+    print(num)
