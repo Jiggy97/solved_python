dfcb16170a82496c686c69a1a4cdd427ec9c05af
jiggy97 <dkswnsdud159@naver.com>
2023-05-24 14:55:16 +0900

    [python] Beakjoon - [silver] 2108 통계학

--- Beakjoon/Silver/2108.py
+++ Beakjoon/Silver/2108.py
@@ -1,14 +1,34 @@
 from collections import Counter
+import sys
 
-n = int(input())
+n = int(sys.stdin.readline())
 n_list = []
 for i in range(n):
-    n_list.append(int(input()))
+    n_list.append(int(sys.stdin.readline()))
 
-counts = Counter(n_list)
-mode = max(counts, key=counts.get)
-print(round(sum(n_list) / n))
 n_list.sort()
-print(n_list[int(n/2)])
-print(mode)
-print(max(n_list) - min(n_list))
+
+mean = round(sum(n_list) / n)
+
+median = n_list[int(n/2)]
+
+# 제출했을 때 틀렸다고 뜨는 코드
+# 이유 > 문제를 잘 읽어보면 두 번째로 작은 최빈값을 출력하라고 돼있음,,ㅅㅂㅅㅂ
+# counts = Counter(n_list)
+# mode_value = max(counts, key=counts.get)
+
+mode = Counter(n_list).most_common()
+mode_value = 0
+if len(mode) == 1:
+    mode_value = mode[0][0]
+elif mode[0][1] == mode[1][1]:
+    mode_value = mode[1][0]
+else:
+    mode_value = mode[0][0]
+
+range_n = max(n_list) - min(n_list)
+
+print(mean)
+print(median)
+print(mode_value)
+print(range_n)
