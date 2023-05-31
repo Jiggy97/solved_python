d3671e45ee6c115822eacd4f35d77e3e5d9084b3
jiggy97 <dkswnsdud159@naver.com>
2023-05-31 22:29:59 +0900

    [python] Beakjoon - [silver] 13305 주유소 (42점 만점에 100점코드 추가)

--- Beakjoon/Silver/13305.py
+++ Beakjoon/Silver/13305.py
@@ -1,19 +1,29 @@
 n = int(input())
+city_dis = list(map(int, input().split()))
+oil_price = list(map(int, input().split()))
 
-city_dis = list(map(int, input().split(" ")))
-
-oil_price = list(map(int, input().split(" ")))
-
-oil_price.pop()
-min_oil = min(oil_price)
-total_dis = sum(city_dis)
 result = 0
-for i in range(n-1):
-    if oil_price[i] == min_oil:
-        result += total_dis * min_oil
-        break
-    else:
-        result += city_dis[i] * oil_price[i]
-        total_dis -= city_dis[i]
+min_oil = oil_price[0]
+for i in range(n - 1):
+    if min_oil > oil_price[i]:
+        min_oil = oil_price[i]
+    result += min_oil * city_dis[i]
 
 print(result)
+
+
+
+
+# 내 첫 풀이
+# oil_price.pop()
+# min_oil = min(oil_price)
+# total_dis = sum(city_dis)
+# result = 0
+# for i in range(n-1):
+#     if oil_price[i] == min_oil:
+#         result += total_dis * min_oil
+#         break
+#     else:
+#         result += city_dis[i] * oil_price[i]
+#         total_dis -= city_dis[i]
+# print(result)
