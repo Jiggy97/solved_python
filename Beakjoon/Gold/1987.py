92d448734a48aa5f058e62f2299a16bd24e7a9d2
안준영 <dkswnsdud159@naver.com>
2023-11-08 20:58:47 +0900

    [python] Beakjoon - [gold] 1987..

--- Beakjoon/Gold/1987.py
+++ Beakjoon/Gold/1987.py
@@ -1,21 +1,33 @@
-def dfs(graph, start, visited, count):
-    count += 1
-    row, col = start
-    visited[row][col] = True
-    for j in graph[row][col]:
-        if not visited[row][col]:
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+result = []
 
+
+def can_move(x, y, visited, alphabet_set):
+    return 0 <= x < r and 0 <= y < c and not visited[x][y] and board[x][y] not in alphabet_set
+
+
+def dfs(x, y, count, alphabet_set):
+    global max_count
+    visited[x][y] = True
+    alphabet_set.add(board[x][y])
+    max_count = max(max_count, count)
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if can_move(nx, ny, visited, alphabet_set):
+            dfs(nx, ny, count + 1, alphabet_set)
+
+    visited[x][y] = False
+    alphabet_set.remove(board[x][y])
 
 
 r, c = map(int, input().split())
-board = [[] for _ in range(r)]
-visit = [[False] * c for _ in range(r)]
-print(board)
-for i in range(r):
-    alphabet = input()
-    for ch in alphabet:
-        board[i].append(ch)
+board = [list(map(str, input())) for _ in range(r)]
+visited = [[False] * c for _ in range(r)]
+alphabet_set = set()
+max_count = 0
 
-result = 0
-dfs(board, (0, 0), visit, result)
-print(result)
+dfs(0, 0, 1, alphabet_set)
+print(max_count)
