c5541e1c1b48b9a656e3ca1fba23fbfd3ba0d595
안준영 <dkswnsdud159@naver.com>
2023-11-13 22:01:20 +0900

    [python] Beakjoon - [silver] 1158

---
+++ Beakjoon/Silver/1158.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+people = list(range(1, n + 1))
+k_index = 0
+result = []
+
+for i in range(n):
+    k_index = (k_index + k - 1) % len(people)
+    result.append(people.pop(k_index))
+
+print('<' + str(result)[1:-1] + '>')
