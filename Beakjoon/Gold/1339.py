5603edf5a8f191205ed0653fcb04a108c5457330
Jiggy97 <dkswnsdud159@naver.com>
2024-03-01 21:58:34 +0900

    [python] Beakjoon - [gold] 1339

--- Beakjoon/Gold/1339.py
+++ Beakjoon/Gold/1339.py
@@ -1,54 +1,22 @@
 n = int(input())
 word_list = []
 alphabet = {}
+
 for _ in range(n):
-    word = input()
-    for w in word:
-        if w in alphabet:
+    word_list.append(input())
+for i in word_list:
+    for j in range(len(i)):
+        digit = len(i) - j - 1
+        if i[j] not in alphabet:
+            alphabet[i[j]] = 10 ** digit
             continue
-        else:
-            alphabet[w] = 0
-    word_list.append(word)
+        alphabet[i[j]] += 10 ** digit
 
-sorted_len = sorted(word_list, key=len, reverse=True)
-result = 0
-value = 9
-for word in sorted_len:
-    string = ''
-    for w in word:
-        if alphabet[w] != 0:
-            string += str(alphabet[w])
-        else:
-            alphabet[w] = value
-            string += str(value)
-            value -= 1
-    result += int(string)
-
-print(result)
-
-# alphabet = {}
-# for _ in range(n):
-#     word = input()
-#     for w in word:
-#         if w in alphabet:
-#             alphabet[w] += 1
-#         else:
-#             alphabet[w] = 1
-#     word_list.append(word)
-#
-# alphabet_desc = dict(sorted(alphabet.items(), key=lambda item: item[1], reverse=True))
-# print(alphabet_desc)
-#
-# value = 9
-# for ap in alphabet_desc:
-#     alphabet[ap] = 9
-#     value -= 1
-#
-# result = 0
-# for word in word_list:
-#     string = ''
-#     for w in word:
-#         string += str(alphabet[w])
-#     result += int(string)
-#
-# print(result)
+alpha_info = list(alphabet.values())
+alpha_info.sort(reverse=True)
+ans = 0
+idx = 0
+for i in range(9, 9-len(alpha_info), -1):
+    ans += alpha_info[idx] * i
+    idx += 1
+print(ans)
