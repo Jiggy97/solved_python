18ac3ecba38a0a95b47aeb1a7544f76d2825a518
Jiggy97 <dkswnsdud159@naver.com>
2024-03-14 15:27:35 +0900

    [python] Beakjoon - [gold] 1916

---
+++ Beakjoon/Gold/1916.py
@@ -0,0 +1,41 @@
+import heapq
+
+INF = int(1e9)
+
+N = int(input())
+M = int(input())
+
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+# print(graph)
+
+start, end = map(int, input().split())
+
+distance = [INF for _ in range(N + 1)]
+
+
+def dijkstra(start):
+    queue = []
+    heapq.heappush(queue, (0, start))
+    distance[start] = 0
+
+    while queue:
+        dist, now = heapq.heappop(queue)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(queue, (cost, i[0]))
+
+
+dijkstra(start)
+
+print(distance[end])
